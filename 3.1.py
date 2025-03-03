132ae3ef408ed993e5c9ca47b44089741f580b36
bodia <telisov401@gmail.com>
2025-03-03 20:48:41 +0200

    1

--- 3.1.py
+++ 3.1.py
@@ -15,6 +15,35 @@ elif b == '/':
 else:
     print('Невірна дія ')
 
+while True:
+    answer = input("Бажаєте продовжити? (так/ні): ")
+
+    if answer.lower() == 'так':
+
+        a = float(input("Введіть перше число :"))
+        b = input("Введіть дію (+, -, *, /): ")
+        c = float(input("Введіть друге число:"))
+
+        if b == '+':
+            print(a + c)
+        elif b == '-':
+            print(a - c)
+        elif b == '*':
+            print(a * c)
+        elif b == '/':
+            if c == 0:
+                print('Ділення на нуль неможливе')
+
+        else:
+            print('Невірна дія ')
+
+    elif answer.lower() == 'ні':
+        print("Бувай.")
+        break
+
+
+
+
 
 
 
